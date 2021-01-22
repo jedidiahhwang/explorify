d1bcac039e60dcf690b1d426dd9ce0f002d5c6ae
Jedidiah <jedidiah.hwang@gmail.com>
2021-01-22 09:42:29 -0700

    hosting setup done, let's try hosting again

--- server/index.js
+++ server/index.js
@@ -28,11 +28,11 @@ app.use(
     })
 );
 
-// app.use(express.static(`${__dirname}` + "/../build"))
+app.use(express.static(`${__dirname}/../build`))
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname + "../build/index.html"))
-// })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+  })
 
 // Auth endpoints
 app.post("/auth/register", authCtrl.register);
